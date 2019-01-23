45dd031266ec1963dedaa6c72778890cb28ea511
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-01-23 15:15:23 +0100

    unecessary files removed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+                <div>
+                    <Card name={robots[0].name} email={ robots[0].email } id={ robots[0].id } />
+                    <Card name={robots[1].name} email={ robots[1].email } id={ robots[1].id } />
+                    <Card name={robots[2].name} email={ robots[2].email } id={ robots[2].id } />
+                    <Card name={robots[3].name} email={ robots[3].email } id={ robots[3].id } />
+                </div>, 
+                document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
