e44c7d4fdb95ecd2ec692bd8ca5401fd5ae36c2a
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-01-24 10:54:42 +0100

    imports added, unecessary import statements removed

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email, id}) => {
     return (
         <div className="bg-light-green dib br3 ma1 pa2 grow bw2 shadow-5">
-            <img src={ `https://robohash.org/${ id }` } alt='avatar photo' />
+            <img src={ `https://robohash.org/${ id }` } alt='avatar' />
             <div>
                 <h2>{ name }</h2>
                 <p>{ email }</p>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import CardList from './CardList';
 import 'tachyons';
 import { robots } from './robots';
 
-ReactDOM.render(
-                <div>
-                    <Card name={robots[0].name} email={ robots[0].email } id={ robots[0].id } />
-                    <Card name={robots[1].name} email={ robots[1].email } id={ robots[1].id } />
-                    <Card name={robots[2].name} email={ robots[2].email } id={ robots[2].id } />
-                    <Card name={robots[3].name} email={ robots[3].email } id={ robots[3].id } />
-                </div>, 
+ReactDOM.render(<CardList robots={ robots } />, 
                 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
