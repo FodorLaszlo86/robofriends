d02ae91af9c57fd8ef2f220dc351b34260ffc3dc
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-01-24 12:18:17 +0100

    dynamic search functionality added

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchField: '',
+            robotFriends: robots
+        }
+        this.onSearchChange = this.onSearchChange.bind(this);
+    }
+
+    onSearchChange(event) {
+        this.setState({searchField: event.target.value});
+    }
+
+    render() {
+        const filteredRobots = this.state.robotFriends.filter(robot => 
+            robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
+        return(
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox searchChange={ this.onSearchChange } />
+                <CardList robots={ filteredRobots } />
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(<CardList robots={ robots } />, 
+ReactDOM.render(<App />, 
                 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
