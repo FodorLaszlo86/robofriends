6b7a892ccfcafa6ceec37ffbd9869cb188a3a424
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-24 14:32:38 +0100

    Updates

---
+++ precache-manifest.d021fe771957c6da586535078ca2f1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "3d804fefac8309f91b56",
+    "url": "/robofriends/static/js/main.3d804fef.chunk.js"
+  },
+  {
+    "revision": "200173c85c3435d9c0eb",
+    "url": "/robofriends/static/js/1.200173c8.chunk.js"
+  },
+  {
+    "revision": "3d804fefac8309f91b56",
+    "url": "/robofriends/static/css/main.4949fa24.chunk.css"
+  },
+  {
+    "revision": "200173c85c3435d9c0eb",
+    "url": "/robofriends/static/css/1.3bf67381.chunk.css"
+  },
+  {
+    "revision": "8e1971abb26c549fc8ad92a1956832bc",
+    "url": "/robofriends/index.html"
+  }
+];
